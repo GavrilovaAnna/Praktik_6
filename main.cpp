2bb42cc787831cdb685d5d5b759475c22a6706ca
GavrilovaAnna <91331145+GavrilovaAnna@users.noreply.github.com>
2022-05-13 20:22:21 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+# включить  " widget.h "
+
+# включить  < QApplication >
+
+int  main ( int argc, char *argv[])
+{
+    QApplication a (argc, argv);
+    виджет ж;
+    ж. показать ();
+    вернуть а. исполнить ();
+}
